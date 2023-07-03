8f880c79a37923a0f93ad9279c84c7fa9acb5026
Hannah Smith <hannahsmith@hannahs-mbp.devices.brown.edu>
2023-07-03 11:55:26 -0400

    BIG PROJECT

--- social_network.py
+++ social_network.py
@@ -1,5 +1,5 @@
 #Various import Statements can go here
-from  social_network_classes import SocialNetwork,Person
+from  social_network_classes import SocialNetwork, Person
 import social_network_ui
 
 
@@ -18,19 +18,144 @@ if __name__ == "__main__":
     while True: 
         if choice == "1":
             print("\nYou are now in the create account menu")
-            ai_social_network.create_account()
+            name = input("What would you like your username to be? ")
+            x = 0
+            while x == 0:
+                for person in ai_social_network.get_name_list():
+                    if person == name:
+                        print("This username already exists!")
+                        name = input("What would you like your username to be? ")
+                    else:
+                        pass
+                x = 1
+            age = input("What is your age? ")
+            while int(age) > 120 or int(age) < 0:
+                print("Please enter a valid age!")
+                age = input("What is your age? ")
+            ai_social_network.create_account(name, age)
 
         elif choice == "2":
+            print("")
+            user = input("What is your username? ")
+            user_object = None
+
+            while True:
+                for person in ai_social_network.get_person_list():
+                    if person.id == user:
+                        user_object = person
+                        break  # Exit the loop once a match is found
+        
+                if user_object is None:
+                    print("This username does not exist!")
+                    user = input("What is your username? ")
+                else:
+                    break
             inner_menu_choice = social_network_ui.manageAccountMenu()
             #Handle inner menu here
             while True:
-                if inner_menu_choice == "5":
+                if inner_menu_choice == "1":
+                    innist_menu_choice = social_network_ui.edit_details_Menu()
+                    while True:
+                        if innist_menu_choice == "1":
+                            new_name = input("What would you like your new username to be? ")
+                            while new_name in ai_social_network.get_name_list():
+                                print("This username already exists")
+                                new_name = input("What would you like your new name to be? ")
+                            user_object.change_name(new_name)
+                            break
+                        elif innist_menu_choice == "2":
+                            new_age = input("What would you like your new age to be?")
+                            user_object.change_age(new_age)
+                            break
+                        
+                        elif innist_menu_choice == '3':
+                            inner_menu_choice = social_network_ui.manageAccountMenu()
+                            break
+                        
+                        else:
+                            innist_menu_choice = social_network_ui.edit_details_Menu()
+                        
+                elif inner_menu_choice == '2':
+                    new_friend = input("Who would you like to add as a friend? ")
+                    friend_object = None
+
+                    while friend_object is None:
+                        for person in ai_social_network.get_person_list():
+                            if person.id == new_friend:
+                                friend_object = person
+
+                    if friend_object is None:
+                        print("This username does not exist!")
+                        new_friend = input("Who would you like to add as a friend? ")
+
+                    user_object.add_friend(friend_object)
+                    print(f"You and {new_friend} are now friends!")
+
                     break
+                
+
+                elif inner_menu_choice == '3':
+                    new_blocked = input("Who would you like to block as a friend? ")
+                    blocked_object = None
+
+                    while blocked_object is None:
+                        for person in ai_social_network.get_person_list():
+                            if person.id == new_blocked:
+                                blocked_object = person
+                                break  # Exit the loop once a match is found
+
+                    if blocked_object is None:
+                        print("This username does not exist!")
+                        new_blocked = input("Who would you like to block as a friend? ")
+                    
+                    user_object.block_friend(blocked_object)
+                    print(f"{new_blocked} is now blocked!")  
+
+                    break
+
+                elif inner_menu_choice == '4':
+                    print("")
+                    print("My friends:")
+                    user_object.view_friends()
+                    
+                    break
+
+                elif inner_menu_choice == '5':
+                    message_user = input("Who would you like to send a message to? ")
+                    message_object = None
+
+
+                    while message_object is None:
+                        for person in ai_social_network.get_person_list():
+                            if person.id == message_user:
+                                message_object = person
+                                break  # Exit the loop once a match is found
+
+                    if message_object is None:
+                        print("This username does not exist!")
+                        message_user = input("Who would you like to send a message to? ")
+
+                    message = input(f"What would you like to say to {message_user}? \n")
+                    user_object.send_message(message_object, message)
+                    print("Your message has been sent!")
+
+                    break 
+
+                elif inner_menu_choice == '6':
+                    print("")
+                    print("My messages:")
+                    user_object.view_messages()
+
+                    break
+
+                elif inner_menu_choice == "7":
+                    break
+
                 else:
                     inner_menu_choice = social_network_ui.manageAccountMenu()
 
         elif choice == "3":
-            print("Thank you for visiting. Goodbye3")
+            print("Thank you for visiting. Goodbye")
             break
 
         else:
@@ -38,7 +163,3 @@ if __name__ == "__main__":
         
         #restart menu
         choice = social_network_ui.mainMenu()
-
-
-
-        
--- social_network_classes.py
+++ social_network_classes.py
@@ -1,7 +1,8 @@
 # A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
 class SocialNetwork:
     def __init__(self):
-        self.list_of_people = [] # this instance variable is initialized to an empty list when social network is created, 
+        self.list_of_people = []
+        self.list_of_names = [] # this instance variable is initialized to an empty list when social network is created, 
                                  # you can save objects of people on the network in this list
         
     ## For more challenge try this
@@ -17,10 +18,23 @@ class SocialNetwork:
         # hint: load a the json file from disk and look up how to recreate the list of people objects.
         pass
 
-    def  create_account(self):
+    def create_account(self, name, age):
         #implement function that creates account here
-        print("Creating ...")
+        self.name = name
+        self.age = age
+        print("Creating Account...")
+        my_new_person = Person(self.name, self.age)
+        self.list_of_people.append(my_new_person)
+        self.list_of_names.append(my_new_person.id)
+        print("Your account has been created!")
         pass
+    def get_person_list(self):
+        return self.list_of_people
+    def get_name_list(self):
+        return self.list_of_names
+
+            
+
 
 
 class Person:
@@ -28,11 +42,49 @@ class Person:
         self.id = name
         self.year = age
         self.friendlist = []
+        self.messages = []
+    
+    def __str__(self):
+        return self.id
+    
 
     def add_friend(self, person_object):
         #implement adding friend. Hint add to self.friendlist
+        self.friendlist.append(person_object) 
         pass
 
-    def send_message(self):
+    def send_message(self, friend_object, message):
         #implement sending message to friend here
+        if friend_object in self.friendlist:
+            friend_object.messages.append('From {self.id}: "{message}"')
+        else:
+            print(f'{friend_object} not in friend list!')
+
         pass
+
+    def block_friend(self, person_object):
+        if person_object in self.friendlist:
+            self.friendlist.remove(person_object)
+        pass
+
+    def view_friends(self):
+        for person in self.friendlist:
+            print(person)
+        pass
+
+    def view_messages(self):
+        for message in self.messages:
+            print(message)
+        pass
+
+    def change_name(self, new_name):
+        self.id = new_name
+        pass
+    
+    def change_age(self, new_age):
+        self.age = new_age
+        pass
+        
+
+    
+        
